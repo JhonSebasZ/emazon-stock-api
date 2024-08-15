76dc218cee2923533de8a704c8c233a225f07c58
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2024-08-15 18:02:33 -0500

    first commit

---
+++ src/main/java/com/emazon/emazon_stock_api/EmazonStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.emazon_stock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonStockApiApplication.class, args);
+	}
+
+}
