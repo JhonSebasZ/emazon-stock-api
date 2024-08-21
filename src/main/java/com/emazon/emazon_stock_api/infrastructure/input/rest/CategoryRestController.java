3705231f096efdaf4fec6424afb21f86b6b38d04
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2024-08-21 11:12:50 -0500

    add documentation in rest controller

--- src/main/java/com/emazon/emazon_stock_api/infrastructure/input/rest/CategoryRestController.java
+++ src/main/java/com/emazon/emazon_stock_api/infrastructure/input/rest/CategoryRestController.java
@@ -3,6 +3,10 @@ package com.emazon.emazon_stock_api.infrastructure.input.rest;
 import com.emazon.emazon_stock_api.application.dto.category.CategoryRequest;
 import com.emazon.emazon_stock_api.application.dto.category.CategoryResponse;
 import com.emazon.emazon_stock_api.application.handler.category.CategoryHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,11 @@ public class CategoryRestController {
 
     private final CategoryHandler categoryHandler;
 
+    @Operation(summary = "Add a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "category already exists", content = @Content)
+    })
     @PostMapping()
     public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest categoryRequest) {
         CategoryResponse categoryResponse = categoryHandler.saveCategory(categoryRequest);
