0ddbdb94015c6ec2af94bd6080c46e49d5435ede
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2024-08-23 07:42:04 -0500

    add documentation

--- src/main/java/com/emazon/emazon_stock_api/infrastructure/input/rest/CategoryRestController.java
+++ src/main/java/com/emazon/emazon_stock_api/infrastructure/input/rest/CategoryRestController.java
@@ -7,7 +7,9 @@ import com.emazon.emazon_stock_api.application.handler.category.CategoryHandler;
 import com.emazon.emazon_stock_api.application.mapper.category.ICategoryResponseMapper;
 import com.emazon.emazon_stock_api.domain.usecase.CategoryUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -25,10 +27,36 @@ public class CategoryRestController {
     private final CategoryHandler categoryHandler;
     private final ICategoryResponseMapper categoryResponseMapper;
 
+    @Operation(
+            summary = "List all categories",
+            description = "Returns a paginated list of all categories with the option to sort by name in ascending or descending order."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of categories", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PageResponse.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid pagination or sort parameters", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @GetMapping()
     public ResponseEntity<PageResponse<CategoryResponse>> getAllCategory(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @Parameter(
+                    description = "Page number (0-based index)",
+                    example = "0",
+                    required = true
+            )
+            @RequestParam() int page,
+            @Parameter(
+                    description = "Number of categories per page",
+                    example = "10",
+                    required = true
+            )
+            @RequestParam() int size,
+            @Parameter(
+                    description = "Sort direction (asc for ascending, desc for descending)",
+                    example = "asc",
+                    required = true
+            )
             @RequestParam(defaultValue = "asc") String sortDirection
     ) {
         PageResponse<CategoryResponse> response = categoryHandler.listCategories(page, size, sortDirection, CategoryUseCase.ORDER_BY);
